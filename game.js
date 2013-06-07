2a7d77a8e198c945bedc84c03760e4ab5496de02
Alice Monday <sparkleshy@gmail.com>
2013-06-07 14:44:44 -0400

    yay, refactoring . plus some pretty stuff

--- game.js
+++ game.js
@@ -1,100 +1,116 @@
 var print = console.log.bind(console)
+var rand = Math.random
 
-var addvv = function(v,w) {var r = []; for (var i=0;i<v.length;i++) r.push(v[i]+w[i]); return r}
-var mulvs = function(v,s) {return v.map(function(v){return v*s})}
+var m = function(m){var t = Array.prototype.slice.apply(arguments); return function(v){return v[m].apply(v,t.slice(1))}}
 
-var canvas = document.getElementById("canvas")
-window.onresize = function(ev) {
+var addvv = function(v,w){var r = []; for (var i=0;i<v.length;i++) r.push(v[i]+w[i]); return r}
+var mulvv = function(v,w){var r = []; for (var i=0;i<v.length;i++) r.push(v[i]*w[i]); return r}
+var addvs = function(v,s){return v.map(function(v){return v+s})}
+var mulvs = function(v,s){return v.map(function(v){return v*s})}
+var divvs = function(v,s){return v.map(function(v){return v/s})}
+var modvs = function(v,s){return v.map(function(v){return v%s})}
+
+var canvas = document.getElementById('canvas')
+canvas.size = function(){return [canvas.width,canvas.height]}
+window.onresize = function(ev){
 	canvas.width = window.innerWidth
 	canvas.height = window.innerHeight}
 window.onresize(null)
 
-var ctx = canvas.getContext("2d")
-
 var image = function(src){
 	var r = new Image()
-	r.ready = false
-	r.onload = function(){r.ready = true}
 	r.src = src
-	r.draw = function(ctx,x,y){if (r.ready) ctx.drawImage(r,x,y);}
+	r.onload = function(){this.draw = function(ctx,x,y){ctx.drawImage(r,x,y)}}
 	return r}
-
-var entity = function(pos,img){
+var entity = function(draw,pos){
 	this.pos = pos
-	this.image = image(img)
-	this.draw = function(ctx){this.image.draw(ctx,this.pos[0],this.pos[1])}
+	this.draw = draw
+	this.alive = function(){return true}
+	
+	this.x = function(){return this.pos[0]}
+	this.y = function(){return this.pos[1]}
+
 	entities.push(this)
 	}
-
 var agent = function(img,pos,hp,update,die){
-	var r = new entity(pos,img)
-	r.vel = [0,0]
+	var r = new entity(img,pos)
 	r.hp = hp
-	r.update = function(delta){
-		if (r.pos[0] < 0 || canvas.width <= r.pos[0] || r.pos[1] < 0 || canvas.height <= r.pos[1])
-			r.hurt(1)
-		if (r.alive()) update(delta)
-		if (r.alive()) {
-			// TODO mulvs
-			r.pos[0] += r.vel[0] * delta
-			r.pos[1] += r.vel[1] * delta
-			}}
-	r.hurt = function(i){r.hp -= i; if (r.hp <= 0) {r.pos = null; r.die()}}
 	r.die = die
-	r.alive = function(){return !!r.pos}
+	r.update = function(delta){
+		if (!(0 <= this.pos[0] && this.pos[0] < canvas.size()[0] && 0 <= this.pos[1] && this.pos[1] < canvas.size()[1]))
+			this.hurt(1)
+		if (this.alive()) update.call(this,delta)
+		if (this.alive()) {this.vel = addvv(this.vel,mulvs(addvs(modvs(this.pos,64),-32),0.0001))}
+		if (this.alive()) {this.pos = addvv(this.pos,mulvs(this.vel,delta))}
+		}
+	r.hurt = function(i){this.hp -= i; if (this.hp <= 0) {this.alive = function(){return false}; this.die()}}
+	r.vel = [0,0]
 	agents.push(r)
 	return r}
 
+var myDrawImage = function(s){var t = image('images/'+s+'.png'); return function(ctx){if (t.draw) t.draw(ctx,this.x(),this.y())}}
+
 var entities = []
-var draw_entities = function(ctx){entities = entities.filter(function(v){if (v.pos) v.draw(ctx); return !!v.pos})}
+var draw_entities = function(ctx){entities = entities.filter(m('alive')); entities.map(m('draw',ctx))}
 var agents = []
-var update_agents = function(delta){agents.slice(0).map(function(v){v.update(delta)}); agents = agents.filter(function(v){return !!v.pos})}
+var update_agents = function(delta){agents.slice(0).map(m('update',delta)); agents = agents.filter(m('alive'))}
 
-new entity([0,0],"images/background2.png")
+new entity(myDrawImage('background2'),[0,0])
 
-var hero = agent("images/hero.png",[canvas.width / 2, canvas.height / 2],100,
-	function(delta){hero.vel = addvv(mulvs(hero.vel, 0.96), mulvs([(keysDown[37]?-1:0)+(keysDown[39]?1:0), (keysDown[38]?-1:0)+(keysDown[40]?1:0)], 0.05))},
+new entity(function(ctx){
+	ctx.strokeStyle = 'rgba(0,0,0,.3)'
+	ctx.lineWidth = 1
+	for (var x = 32; x < canvas.width; x += 64)
+		ctx.drawLine([x,0],[x,canvas.height])
+	for (var y = 32; y < canvas.height; y += 64)
+		ctx.drawLine([0,y],[canvas.width,y])
+	},null)
+new entity(function(ctx){
+	ctx.fillStyle = 'white'
+	ctx.font = '24px Consolas'
+	ctx.textBaseline = 'top'
+	ctx.fillText('entities: '+entities.length,32,32)
+	ctx.fillText('hero.hp: '+hero.hp,32,64)
+	},null)
+
+var hero = agent(myDrawImage('hero'),divvs(canvas.size(),2),100,
+	function(delta){hero.vel = addvv(mulvs(hero.vel,0.96), mulvs([(keys_down[37]?-1:0)+(keys_down[39]?1:0), (keys_down[38]?-1:0)+(keys_down[40]?1:0)], 0.05))},
 	function(){})
 
 var monster = function(){
-	var r = agent("images/monster.png",[
-		Math.random() * (canvas.width  - 64),
-		Math.random() * (canvas.height - 64)
-		],1,
+	agent(myDrawImage('monster'),mulvv(canvas.size(),[rand(),rand()]),5,
 		function(delta){
-			r.vel[0] += (Math.random()-0.5) * 0.01 * delta
-			r.vel[1] += (Math.random()-0.5) * 0.01 * delta
+			this.vel[0] += (Math.random()-0.5) * 0.005 * delta
+			this.vel[1] += (Math.random()-0.5) * 0.005 * delta
 			if (hero.alive()) {
-				if (r.pos[0] - 64 <= hero.pos[0] && hero.pos[0] <= r.pos[0] + 64 &&
-					r.pos[1] - 64 <= hero.pos[1] && hero.pos[1] <= r.pos[1] + 64)
-						{r.hurt(1); hero.hurt(1)}
+				if (this.pos[0] - 64 <= hero.pos[0] && hero.pos[0] <= this.pos[0] + 64 &&
+					this.pos[1] - 64 <= hero.pos[1] && hero.pos[1] <= this.pos[1] + 64)
+						{this.hurt(1); hero.hurt(1)}
 				}
 			},
 		function(){monster(); if (Math.random() < 0.1) monster()}
-		)
-	return r}
+		)}
 
 // Handle keyboard controls
-var keysDown = {}
-addEventListener("keydown", function(e){keysDown[e.keyCode] = true}, false)
-addEventListener("keyup"  , function(e){delete keysDown[e.keyCode]}, false)
+var keys_down = {}
+addEventListener('keydown',function(e){keys_down[e.keyCode] = true},false)
+addEventListener('keyup'  ,function(e){delete keys_down[e.keyCode]},false)
 
-// Draw everything
-var render = function(){
-	draw_entities(ctx)
+;(function(){
+	var ctx = canvas.getContext('2d')
+	ctx.drawLine = function(a,b){
+		this.beginPath()
+		this.moveTo(a[0]+0.5,a[1]+0.5)
+		this.lineTo(b[0]+0.5,b[1]+0.5)
+		this.stroke()
+		this.closePath()}
 
-	ctx.fillStyle = "white"
-	ctx.font = "24px Consolas"
-	ctx.textBaseline = "top"
-	ctx.fillText("entities: "+entities.length, 32, 32)
-	ctx.fillText("hero.hp: "+hero.hp, 32, 64)
-	}
-
-for (var i = 0; i < 100; i++) monster()
-var then = Date.now()
-setInterval(function(){
-	var now = Date.now()
-	update_agents(now - then)
-	render()
-	then = now
-	}, 1) // Execute as fast as possible
+	for (var i = 0; i < 15; i++) monster()
+	var then = Date.now()
+	setInterval(function(){
+		var now = Date.now()
+		update_agents(now - then)
+		draw_entities(ctx)
+		then = now
+		},1) // Execute as fast as possible
+	}())
