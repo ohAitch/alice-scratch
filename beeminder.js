0b4d4f12cd5c4346aeb7d1696e24c6630699eb82
alice0meta <alice0meta@gmail.com>
2014-02-16 02:20:59 -0500

    fix bug with bad input
    also, remove a debugging command

--- beeminder.js
+++ beeminder.js
@@ -25,9 +25,8 @@ function scroll_to(v) {
 
 	// on this specific goal, display most recent datapoint
 	if (location.href === 'https://www.beeminder.com/alice0meta/goals/team') {
-		$('#datapoint-msg').remove()
 		var t = $('.recent-data').find('span').last()
-		var data = t.attr('data-comment').match(/\| (.*)/)[1]
+		var data = (t.attr('data-comment').match(/\| (.*)/) || ['','< no request :c >'])[1]
 		var day = parseInt(t.text().match(/^(\d+)/)[1])
 		var msg = 'alice-'+(new Date().getDate()-1 === day? 'yesterday' : day)+' would like you to:\n'+data
 		$('body').append($('<div id="datapoint-msg" style="top:20px; left:20px; position:fixed; max-width:400px; font-family:monospace; text-align:left; padding:0px 3px; border: 1px solid #000; background-color:rgba(255,255,255,.7); white-space:pre-wrap">').text(msg))
