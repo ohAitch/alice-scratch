f0f3a06f5765e85ffb7b234829b98a97664e992c
alice0meta <alice0meta@gmail.com>
2014-02-16 02:05:06 -0500

    scrolling no longer screws with history
    or address bar, even!

--- lw_unread.js
+++ lw_unread.js
@@ -7,32 +7,39 @@
 
 // may break on dynamically loaded new comments
 
-(function($){
-	function char(v){return typeof v === 'number'? v : v.charCodeAt(0)}
-	function on_key(key,f){$(window).keydown(function(e){if (e.which === char(key)) f(e)})}
-	function mod(a,b){return ((a%b)+b) % b}
+function scroll_to(v) {
+	// this may be rather hacky and bad, unsure
+	var t = location.href
+	location.replace(typeof v === 'string'? v.replace(/^#?/,'#') : '#'+v.id)
+	window.history.replaceState(null,null,t)
+}
 
-	var ids = ['#'].concat($.map($('.new-comment'),function(v){return '#'+v.id}))
-	if (ids.length === 1) return
+(function($){
+	function char(v) {return typeof v === 'number'? v : v.charCodeAt(0)}
+	function on_key(key,f) {$(window).keydown(function(e){if (e.which === char(key)) f(e)})}
+	function mod(a,b) {return ((a%b)+b) % b}
+
+	var new_comments = $('.new-comment')
+	if (new_comments.length === 0) return
 
 	var button = $('<input id="next-unread" type="button" style="top:20px; left:20px; position:fixed; font-family:monospace; background-color:white; text-align:left">')
 
-	var at = 0
-	function update_button(){button[0].value = 'unread('+at+'/'+(ids.length-1)+')\nkeys: [←] [→]'}
-	function update(prev_at){
-		location.href = ids[at]
-		$(ids[prev_at]).removeClass('at-comment')
-		$(ids[at]).addClass('at-comment')
+	var at = -1
+	function update_button() {button[0].value = 'unread('+(at+1)+'/'+new_comments.length+')\nkeys: [←] [→]'}
+	function update(prev_at) {
+		scroll_to(new_comments[at])
 		// lw uses !important, which is hard to override :c
-		$(ids[prev_at]).addClass('new-comment')
-		$(ids[at]).removeClass('new-comment')
+		$(new_comments[at]).addClass('at-comment')
+		$(new_comments[prev_at]).addClass('new-comment')
+		$(new_comments[prev_at]).removeClass('at-comment')
+		$(new_comments[at]).removeClass('new-comment')
 		update_button()
 	}
 
 	update_button()
 
-	on_key(37,                     function(){var t = at; at=mod(at-1, ids.length); update(t)}) // previous
-	on_key(39, button[0].onclick = function(){var t = at; at=mod(at+1, ids.length); update(t)}) // next
+	on_key(37,                     function(){var t = at; at=mod(at-1, new_comments.length); update(t)}) // previous
+	on_key(39, button[0].onclick = function(){var t = at; at=mod(at+1, new_comments.length); update(t)}) // next
 
 	$('head').append($('<style type="text/css"> .at-comment {'+
 		'border: 5px solid #ee90ee !important;'+
