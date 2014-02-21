bddadd84d647ae177820941c826c27be4f21d261
alice0meta <alice0meta@gmail.com>
2014-02-21 14:52:42 -0500

    bugs, misc

--- beeminder.js
+++ beeminder.js
@@ -5,6 +5,9 @@
 // @match      http*://*.beeminder.com/*
 // ==/UserScript==
 
+// bug: when beeminder gives me a notification, i don't see it, and then when it goes away, i'm not scrolled to the right location . also, the notifications are annoying
+// bug: the 'yesterday' flag doesn't refresh when the date changes
+
 /////  THE FOLLOWING IS COPIED FROM ELSEWHERE  ///// lw_unread.js
 function scroll_to(v) {
 	// this may be rather hacky and bad, unsure
@@ -28,7 +31,7 @@ function scroll_to(v) {
 		var t = $('.recent-data').find('span').last()
 		var data = (t.attr('data-comment').match(/\| (.*)/) || ['','< no request :c >'])[1]
 		var day = parseInt(t.text().match(/^(\d+)/)[1])
-		var msg = 'alice-'+(new Date().getDate()-1 === day? 'yesterday' : day)+' would like you to:\n'+data
+		var msg = 'alice-'+(new Date().getDate()-1 === day? 'yesterday' : day)+' asks:\n'+data
 		$('body').append($('<div id="datapoint-msg" style="top:20px; left:20px; position:fixed; max-width:400px; font-family:monospace; text-align:left; padding:0px 3px; border: 1px solid #000; background-color:rgba(255,255,255,.7); white-space:pre-wrap">').text(msg))
 	}
 })(jQuery)
