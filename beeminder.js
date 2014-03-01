4d4820289b6fe35a5bcfcdc6a594946ec55f7bd4
alice0meta <alice0meta@gmail.com>
2014-03-01 18:15:36 -0500

    fixed a couple bugs

--- beeminder.js
+++ beeminder.js
@@ -6,7 +6,6 @@
 // ==/UserScript==
 
 // bug: when beeminder gives me a notification, i don't see it, and then when it goes away, i'm not scrolled to the right location . also, the notifications are annoying
-// bug: the 'yesterday' flag doesn't refresh when the date changes
 
 /////  THE FOLLOWING IS COPIED FROM ELSEWHERE  ///// lw_unread.js
 function scroll_to(v) {
@@ -16,7 +15,23 @@ function scroll_to(v) {
 	window.history.replaceState(null,null,t)
 }
 
-(function($){
+// experimental
+function s(v,s) {return eval('(function(v){'+s.replace(/\./g,'v.')+';return v})')(v)}
+
+// time
+Date.now_s = function(){return Date.now() / 1000}
+var run = {
+	in:   function(s,f){return {id:setTimeout( f,s*1000), cancel:function(){cancelTimeout( this.id)}}},
+	every:function(s,f){return {id:setInterval(f,s*1000), cancel:function(){cancelInterval(this.id)}}},
+	tomorrow:function(f){
+		var start = new Date()
+		var cancel = false
+		var r = run.in(new Date(start).setHours(24,0,0,0)/1000 - start/1000,
+			function t(){if (new Date().getDate() === start.getDate()) run.in(1,t); else {if (!cancel) f()}})
+		return {cancel:function(){cancel = true; r.cancel();}}}
+}
+
+;(function($){
 	// style
 	$('.delete_datapoint').css({padding:'0px 1em'})
 	
@@ -31,7 +46,9 @@ function scroll_to(v) {
 		var t = $('.recent-data').find('span').last()
 		var data = (t.attr('data-comment').match(/\| (.*)/) || ['','< no request :c >'])[1]
 		var day = parseInt(t.text().match(/^(\d+)/)[1])
-		var msg = 'alice-'+(new Date().getDate()-1 === day? 'yesterday' : day)+' asks:\n'+data
-		$('body').append($('<div id="datapoint-msg" style="top:20px; left:20px; position:fixed; max-width:400px; font-family:monospace; text-align:left; padding:0px 3px; border: 1px solid #000; background-color:rgba(255,255,255,.7); white-space:pre-wrap">').text(msg))
+		$('body').append($('<div id="datapoint-msg" style="top:20px; left:20px; position:fixed; max-width:400px; font-family:monospace; text-align:left; padding:0px 3px; border: 1px solid #000; background-color:rgba(255,255,255,.7); white-space:pre-wrap">'))
+		;(function t(){
+			$('#datapoint-msg').text('alice-'+(s(new Date(),'.setDate(.getDate()-1)').getDate() === day? 'yesterday' : day)+' asks:\n'+data)
+			run.tomorrow(t)})()
 	}
 })(jQuery)
