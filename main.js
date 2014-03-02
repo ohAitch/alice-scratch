c39b6c933d4d72fd81dd15309850cd192c6645da
alice0meta <alice0meta@gmail.com>
2014-03-01 21:36:58 -0500

    hello world

---
+++ main.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+var fs = require("fs")
+
+var argv = process.argv.slice(2)
+
+function print(v) {console.log(v)}
+
+/*
+var tasks = {}
+checkFile = function (f) {
+	// create a pattern to match on
+	var pattern = /(todo|fixme|optimise|optimize)\W*(.*$)/i
+	var items = []
+	var lineNumber = 1
+	file = fs.readFileSync(f).toString().split('\n')
+	file.forEach(function (line) {
+		if (match = line.match(pattern)) {
+			items.push(" * [" + lineNumber.toString() + "] " + match[1].toUpperCase() + ": " + match[2])
+		}
+		lineNumber++
+	})
+	if (items.length > 0) { // Output the file name
+		console.log(f) // Output the matches
+		items.forEach(function (item) {
+			console.log(item)
+		})
+	}
+}
+tasks.search = function (dir, action) { // Assert that it's a function
+	if (typeof action !== "function") action = function (error, file) {} // Read the directory
+	fs.readdir(dir, function (err, list) { // Return the error if something went wrong
+		if (err) return action(err) // For every file in the list
+		list.forEach(function (file) { // Full path of that file
+			var path = dir + "/" + file // Get the file's stats
+			fs.stat(path, function (err, stat) {
+				//console.log(path + " is a file? " + stat.isFile())
+				// If the item is a directory
+				if (stat && stat.isDirectory()) {
+					tasks.search(path, action)
+				} else if (stat && stat.isFile()) {
+					checkFile(path)
+				} else {
+					action(null, path)
+				}
+			})
+		})
+	})
+}
+// If no arguments are passed, pass the current directory
+if (typeof argv[0] === "undefined" || argv[0] === null) {
+	argv[0] = "."
+}
+if (argv[0] === "--help" || argv[0] === "-h") {
+	console.log("usage: net-notes [options] \n\n" + "Searches files for TODO, FIXME or OPTIMISE tags within your project,\n" + "displaying the line number and file name along with the tag description\n" + "options:\n" + "[directory_name] # Optional parameter, will run from current directory\n" + "-h, [--help] # Show this help message and quit")
+} else {
+	tasks.search(argv[0])
+}
+*/
+
+
+
+/*
+var ExtendedMath
+ExtendedMath = (function () {
+	function ExtendedMath() {}
+	// TODO: Allow number to be passed in to call square root on
+	ExtendedMath.prototype.root = function () {
+		return Math.sqrt
+	}
+	//FIXME: Squaring currently cubes the result which is incorrect
+	ExtendedMath.prototype.square = function (x) {
+		return square(x) * x
+	}
+	ExtendedMath.prototype.cube = function (x) {
+		return x * square(x)
+	}
+	//TODO: Add fix for JavaScript floating point calculations
+	return ExtendedMath
+})()
+*/
