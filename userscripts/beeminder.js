fae741a5f93b6c477ab97edad91bd54867ebe134
alice0meta <alice0meta@gmail.com>
2014-03-23 15:35:37 -0400

    misc

--- userscripts/beeminder.js
+++ userscripts/beeminder.js
@@ -5,7 +5,7 @@
 // @match      http*://*.beeminder.com/*
 // ==/UserScript==
 
-// bug: when beeminder gives me a notification, i don't see it, and then when it goes away, i'm not scrolled to the right location . also, the notifications are annoying
+// todo: fix this: when beeminder gives me a notification, i don't see it, and then when it goes away, i'm not scrolled to the right location . also, the notifications are annoying
 
 /////  THE FOLLOWING IS COPIED FROM ELSEWHERE  ///// lw_unread.js
 function scroll_to(v) {
@@ -39,17 +39,20 @@ var run = {
 	// if #goal-user exists, scroll to it
 	if ($('#goal-user')[0]) scroll_to('goal-user')
 
-	// get rid of the datapoint deletion dialog
+	// remove datapoint deletion dialog
 	$('.delete_datapoint').removeAttr('data-confirm')
+
+	// remove dogfood mandate box
+	if ($('#admin-links').children('.content-container').children('h3').text()==="Dogfood Mandate")
+		$('#admin-links').remove()
 
 	// on this specific goal, display most recent datapoint
 	if (location.href === 'https://www.beeminder.com/alice0meta/goals/team') {
-		var t = $('.recent-data').find('span').last()
-		var data = (t.attr('data-comment').match(/\| (.*)/) || ['','< no request :c >'])[1]
-		var day = parseInt(t.text().match(/^(\d+)/)[1])
-		$('body').append($('<div id="datapoint-msg" style="top:20px; left:20px; position:fixed; max-width:400px; font-family:monospace; text-align:left; padding:0px 3px; border: 1px solid #000; background-color:rgba(255,255,255,.7); white-space:pre-wrap">'))
+		//'< no request :c >'
+		var t = $('.recent-data').find('span').map(function(v){var t = v.attr('data-comment').match(/\| (.*)/); return t?[t[1],parseInt(v.text().match(/^(\d+)/)[1])]:undefined}).filter(function(v){return v}).slice(-1)[0]; var data = t[0]; var day = t[1]
+		var msg; $('body').append(msg = $('<div id="datapoint-msg" style="top:20px; left:20px; position:fixed; max-width:400px; font-family:monospace; text-align:left; padding:0px 3px; border: 1px solid #000; background-color:rgba(255,255,255,.7); white-space:pre-wrap">'))
 		;(function t(){
-			$('#datapoint-msg').text('alice-'+(s(new Date(),'.setDate(.getDate()-1)').getDate() === day? 'yesterday' : day)+' asks:\n'+data)
+			msg.text('alice-'+(s(new Date(),'.setDate(.getDate()-1)').getDate() === day? 'yesterday' : day)+' asks:\n'+data)
 			run.tomorrow(t)})()
 	}
 })(jQuery)
