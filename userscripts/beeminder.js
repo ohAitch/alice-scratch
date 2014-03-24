7d96b7082b96fd875ec43bc012f7b71bd8f3dfb6
alice0meta <alice0meta@gmail.com>
2014-03-24 00:07:04 -0400

    woo! begone, blasted header!

--- userscripts/beeminder.js
+++ userscripts/beeminder.js
@@ -5,19 +5,18 @@
 // @match      http*://*.beeminder.com/*
 // ==/UserScript==
 
-// todo: fix this: when beeminder gives me a notification, i don't see it, and then when it goes away, i'm not scrolled to the right location . also, the notifications are annoying
+// todo: add checks so that things get executed only on the right pages
+// todo‽: consider writing an svg graph generator
 
 /////  THE FOLLOWING IS COPIED FROM ELSEWHERE  ///// lw_unread.js
-function scroll_to(v) {
+/*function scroll_to(v) {
 	// this may be rather hacky and bad, unsure
 	var t = location.href
 	location.replace(typeof v === 'string'? v.replace(/^#?/,'#') : '#'+v.id)
 	window.history.replaceState(null,null,t)
-}
-
+}*/
 // experimental
 function s(v,s) {return eval('(function(v){'+s.replace(/\./g,'v.')+';return v})')(v)}
-
 /////  THE FOLLOWING IS COPIED FROM ELSEWHERE  ///// lang-alpha
 // time
 Date.now_s = function(){return Date.now() / 1000}
@@ -37,7 +36,11 @@ var run = {
 	$('.delete_datapoint').css({padding:'0px 1em'})
 	
 	// if #goal-user exists, scroll to it
-	if ($('#goal-user')[0]) scroll_to('goal-user')
+	//if ($('#goal-user')[0]) scroll_to('goal-user')
+
+	// get rid of header
+	$('#header').remove()
+	$('.content').css({'padding-top': '0px'})
 
 	// remove datapoint deletion dialog
 	$('.delete_datapoint').removeAttr('data-confirm')
