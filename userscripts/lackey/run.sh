859797accb087695f1e61d651d41fbbed130410e
Alice Monday <alice0meta@gmail.com>
2014-04-29 10:20:01 -0700

    beginning lackey

--- userscripts/lackey/run.sh
+++ userscripts/lackey/run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 eval $mydir
 
-cp private.xml ~/"Library/Application Support/KeyRemap4MacBook/private.xml"
+cp private.xml ~/"Library/Application Support/KeyRemap4MacBook/"
 "/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli" reloadxml
 "/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli" enable lackey
+
+cp lackey.keylayout ~/"Library/Keyboard Layouts/"
