24a93bb5380bae307fbee7d01fb323e7727207db
Alice Monday <alice0meta@gmail.com>
2014-04-29 18:28:53 -0700

    that's ... that's a good day's work.
    enough for now.

--- userscripts/lackey/run.sh
+++ userscripts/lackey/run.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 eval $mydir
 
+cp lackey.icns ~/"Library/Keyboard Layouts/"
+make_keylayout.js
+
 cp private.xml ~/"Library/Application Support/KeyRemap4MacBook/"
 "/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli" reloadxml
 "/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli" enable lackey
-
-cp lackey.keylayout ~/"Library/Keyboard Layouts/"
