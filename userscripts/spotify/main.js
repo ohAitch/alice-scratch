893c40bff4bdfa4f023f9c8415d289f020cf730b
Alice Monday <alice0meta@gmail.com>
2014-04-29 19:55:28 -0700

    oh my god it works now

--- userscripts/spotify/main.js
+++ userscripts/spotify/main.js
@@ -1,13 +1,33 @@
 #!/usr/bin/env node
 
 var fs = require('fs')
-var spotify = require('spotify')({appkeyFile: 'spotify_appkeyFile.key'})
+var spotify = require('node-spotify')({appkeyFile: process.env.HOME+'/.spotiman/spotify_appkey.key'})
 
-var auth = JSON.parse(fs.readFileSync('auth.json'))
+var print = console.log.bind(console)
 
+var auth = JSON.parse(fs.readFileSync(process.env.HOME+'/.spotiman/auth.json'))
+
+var t = Date.now()/1000
 spotify.ready(function() {
-	var starredTracks = spotify.playlistContainer.getStarred().getTracks()
-	spotify.player.play(starredTracks[0])
+	print('ready!',Date.now()/1000-t)
+	var l = spotify.playlistContainer.getPlaylists()
+	l.forEach(function(v,i){
+		var f = function(){
+			if (v.isLoaded) print('playlist!',i,v)
+			else setTimeout(f,1000)
+		}
+		setTimeout(f,1000)
 	})
-
+})
 spotify.login(auth.username, auth.password, false, false)
+
+/*isLoaded
+owner
+on
+off
+getPlaylists
+getStarred
+addPlaylist
+addFolder
+deletePlaylist
+movePlaylist*/
