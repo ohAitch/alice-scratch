3b126034b3c4bed17b78c2f31eada58ba60213af
Alice Monday <alice0meta@gmail.com>
2014-05-09 19:58:00 -0700

    lots of changes in lackey!

--- lackey/run.sh
+++ lackey/run.sh
@@ -4,6 +4,6 @@ eval $mydir
 cp lackey.icns ~/"Library/Keyboard Layouts/"
 make_keylayout.js
 
-make_krmb_xml.js alice.lackey > ~/"Library/Application Support/KeyRemap4MacBook/private.xml"
+make_krmb.js alice.lackey > ~/"Library/Application Support/KeyRemap4MacBook/private.xml"
 "/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli" reloadxml
 "/Applications/KeyRemap4MacBook.app/Contents/Applications/KeyRemap4MacBook_cli.app/Contents/MacOS/KeyRemap4MacBook_cli" enable lackey
