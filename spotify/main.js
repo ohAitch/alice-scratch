e625e589dd0f324b1b727b9963ca38c9e5ed059f
Alice Monday <alice0meta@gmail.com>
2014-05-11 13:29:43 -0700

    now much less asynchronous. maybe this is not good

--- spotify/main.js
+++ spotify/main.js
@@ -3,9 +3,15 @@
 var F = function(v){return v[0]==='~'? process.env.HOME+v.slice(1) : v}
 
 var fs = require('fs')
+
 var spotify = require('node-spotify')({appkeyFile: F('~/.spotiman/spotify_appkey.key')})
 var _ = require('underscore')
 var m = require('moment')
+var minimist = require('minimist')
+var sync = require('sync')
+
+var err_print = function(f){return function(){try{f()} catch (e) {console.log('error!',e,e.message,e.stack)}}}
+sync(err_print(function(){
 
 // http://gizmodo.com/you-can-download-any-spotify-song-as-an-mp3-with-this-c-494493386 ??
 
@@ -14,84 +20,85 @@ Array.prototype.group_by = function(f){var r = {}; this.map(function(v){var t; (
 var seq = function(v){return typeof v === 'string'? v.split('') : v instanceof Array? v : Object.keys(v).map(function(k){return [k,v[k]]})}
 Array.prototype.m_concat = function(){return Array.prototype.concat.apply([],this)}
 var object = function(v){return v.reduce(function(r,v){r[v[0]] = v[1]; return r},{})}
+var C = function(v){return function(){return v}}
+
+var args = minimist(process.argv.slice(2))
 
 var auth = JSON.parse(fs.readFileSync(F('~/.spotiman/auth.json')))
 
 var timer = function λ(){var t = Date.now()/1000; var r = t - λ.now; λ.now = t; return Math.round(r*100)/100+'s'}; timer()
+var poll = function(f,cb){(function λ(){if (f()) cb(); else setTimeout(λ,50)})()}
 
-var stringify_tracks = function(tracks){
-	tracks = JSON.parse(JSON.stringify(tracks))
-	tracks.map(function(v){
-		delete(v.isLoaded)
-		delete(v.popularity)
-		delete(v.starred)
-		delete(v.album.isLoaded)
-		delete(v.duration)
-		v.artists.map(function(v){delete(v.isLoaded)})
-		v.playlists = v.playlists.map(function(v){return v.name})
-		})
-	return JSON.stringify(tracks,null,'\t')}
-
-spotify.ready(function(){
-	print('spotify ready!',timer())
+var login = function(){
+	spotify.login(auth.username, auth.password, false, false)
+	spotify.ready.sync()
+	login = C()
+	print('spotify ready!',timer()) }
+var playlists_ = function(){
+	login()
 	var l = spotify.playlistContainer.getPlaylists()
-	;(function λ(){
-		if (l.every(function(v){return v.isLoaded || v.type})) {
-			print('playlists loaded!',timer())
-			var playlists = []; var tags = []; var out = playlists
-			l.filter(function(v){return v.name!=='-'}).map(function(v){if (v.isLoaded) out.push(v); else if (v.name==='tags') out = tags; else out = playlists})
-			var tracks = []
-			playlists.map(function(playlist){playlist.getTracks().map(function(v){
-				(v.playlists=v.playlists||[]).push(playlist)
-				tracks.push(v)
-			})})
-			;(function λ(){
-				if (tracks.every(function(v){return v.isLoaded})) {
-					print('tracks loaded!',timer())
+	poll.sync(null,function(){return l.every(function(v){return v.isLoaded || v.type})})
+	var r_p = []; var r_t = []; var out = r_p
+	l.filter(function(v){return v.name!=='-'}).map(function(v){if (v.isLoaded) out.push(v); else if (v.name==='tags') out = r_t; else out = r_p})
+	playlists = C(r_p)
+	tags = C(r_t)
+	print('playlists loaded!',timer()) }
+var playlists = function(){playlists_(); return playlists()}
+var tags = function(){playlists_(); return tags()}
+var tracks = function(){
+	var r = []
+	playlists().map(function(playlist){playlist.getTracks().map(function(v){
+		(v.playlists=v.playlists||[]).push(playlist)
+		r.push(v)
+	})})
+	poll.sync(null,function(){return r.every(function(v){return v.isLoaded})})
+	tracks = C(r)
+	print('tracks loaded!',timer())
+	return r}
 
-					fs.writeFileSync(F('~/ali/history/spotify/'+m().toISOString()+'.json'),stringify_tracks(tracks))
-					print('history saved!',timer())
+var main = function(){
+	var tr = tracks()
+	tr = seq(tr.group_by(function(v){return v.link})).map(function(v){v[1][0].playlists = _.uniq(v[1].map(function(v){return v.playlists}).m_concat(),'link'); return v[1][0]})
+	tr = seq(tr.group_by(function(v){return v.artists[0].name===''? v.name : v.artists[0].name+' @ '+v.name}))
+		//.map(function(v){return v[1].length===1? [v] : seq(v[1].group_by(function(v){return v.artists[0].name+' @ '+v.name}))}).m_concat()
+		.map(function(v){return v[1].length===1? [v] : seq(v[1].group_by(function(v){return v.artists[0].name+' @ '+v.album.name+' @ '+v.name}))}).m_concat()
 
-					tracks = seq(tracks.group_by(function(v){return v.link})).map(function(v){v[1][0].playlists = _.uniq(v[1].map(function(v){return v.playlists}).m_concat(),'link'); return v[1][0]})
-					tracks = seq(tracks.group_by(function(v){return v.artists[0].name===''? v.name : v.artists[0].name+' @ '+v.name}))
-						//.map(function(v){return v[1].length===1? [v] : seq(v[1].group_by(function(v){return v.artists[0].name+' @ '+v.name}))}).m_concat()
-						.map(function(v){return v[1].length===1? [v] : seq(v[1].group_by(function(v){return v.artists[0].name+' @ '+v.album.name+' @ '+v.name}))}).m_concat()
+	// okay . we've generated an unique name for all tracks. what next?
+	// i think maybe the *first* thing we should do / should've done is just add version control
+	// if we stick with nodespotify's format, we can do this really simply by just saving the json
+	// we might sorta care about dates ... but eh ... screw them, it's too hard to keep track of that bullshit
+	// aaand done!
 
-					// okay . we've generated an unique name for all tracks. what next?
-					// i think maybe the *first* thing we should do / should've done is just add version control
-					// if we stick with nodespotify's format, we can do this really simply by just saving the json
-					// we might sorta care about dates ... but eh ... screw them, it's too hard to keep track of that bullshit
-					// aaand done!
+	Object.keys(object(tr)).map(function(v,i){if (i < 10) print(v)})
+	}
 
+var save_history = function(){
+	var stringify_tracks = function(tracks){
+		tracks = JSON.parse(JSON.stringify(tracks))
+		tracks.map(function(v){
+			delete(v.isLoaded)
+			delete(v.popularity)
+			delete(v.starred)
+			delete(v.album.isLoaded)
+			delete(v.duration)
+			v.artists.map(function(v){delete(v.isLoaded)})
+			v.playlists = v.playlists.map(function(v){return v.name})
+			})
+		return JSON.stringify(tracks,null,'\t')}
+	fs.writeFileSync(F('~/ali/history/spotify/'+m().toISOString()+'.json'),stringify_tracks(tracks()))
+	print('history saved!',timer()) }
 
+//===----------------===// call function based on args //===---------------===//
 
-					//Object.keys(object(tracks)).map(function(v){print(v)})
-					/*tracks.map(function(v){
-						print(v)
-						{ isLoaded: true,
-						  popularity: 0,
-						  starred: true,
-						  album: 
-						   { isLoaded: true,
-						     link: 'spotify:album:0000000000000000000000',
-						     name: '' },
-						  artists: 
-						   [ { isLoaded: true,
-						       link: 'spotify:artist:0000000000000000000000',
-						       name: '' } ],
-						  duration: 320,
-						  link: 'spotify:local:::Elyot+Grant+-+Epic+Battle:320',
-						  name: 'Elyot Grant - Epic Battle' }
-						//if (v.album.name.match(/→/) || v.name.match(/→/)) print('what',v)
-					})*/
-					/*tracks.map(function(v,i){
-						if ((v.artists[0].name).match(/@/) || v.album.name.match(/@/) || v.name.match(/@/)) print(i,v)
-					})*/
-					process.exit()
-				} else setTimeout(λ,50) })()
-		} else setTimeout(λ,50) })()
-})
-spotify.login(auth.username, auth.password, false, false)
+if (module.parent) print("oh my goodness, so sorry, but, spotiman isn't built to be require()'d!")
+else switch (args._[0]) {
+case undefined     : main(); break
+case 'save-history': save_history(); break
+}
+
+//===---------------------------===// <end> //===--------------------------===//
+
+process.exit()
 
 /*
 track: name [tag]
@@ -104,27 +111,4 @@ tag:
 	playlist-in-tags
 */
 
-/*
-??:
-	isLoaded
-	owner
-	on
-	off
-	getPlaylists
-	getStarred
-	addPlaylist
-	addFolder
-	deletePlaylist
-	movePlaylist
-playlist:
-	isLoaded
-	description
-	link
-	name
-	on
-	off
-	getTracks
-	addTracks
-	removeTracks
-	reorderTracks
-	*/
+}))
