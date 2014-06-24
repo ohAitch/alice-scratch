7e29c3faefc9dba7e7cb972927210ec1a401b9c2
Alice Monday <alice0meta@gmail.com>
2014-06-23 23:37:15 -0700

    rename many things

--- ξ/run.sh
+++ ξ/run.sh
@@ -3,35 +3,35 @@ eval $mydir
 
 rm -r tmp; mkdir tmp; mkdir tmp/1; mkdir tmp/2; mkdir tmp/3
 
-exr     main.ξc.js   main.ξ tmp
-exr tmp/main.ξc.js   main.ξ tmp/1
-exr tmp/1/main.ξc.js main.ξ tmp/2
-exr tmp/2/main.ξc.js main.ξ tmp/3
+exr     ξ.ξc.js   ξ.ξ tmp
+exr tmp/ξ.ξc.js   ξ.ξ tmp/1
+exr tmp/1/ξ.ξc.js ξ.ξ tmp/2
+exr tmp/2/ξ.ξc.js ξ.ξ tmp/3
 if [ $? = 0 ]; then
 
 echo '--- success ξ ---'
-cp main.ξc.js main.ξc.js.bak
-cp tmp/3/main.ξc.js main.ξc.js
+cp ξ.ξc.js ξ.ξc.js.bak
+cp tmp/3/ξ.ξc.js ξ.ξc.js
 
-exr       main.ηc.ξc.js main.η tmp  ; exr main.ξc.js   tmp/main.ηc.ξ .
-exr   tmp/main.ηc.ξc.js main.η tmp/1; exr main.ξc.js tmp/1/main.ηc.ξ .
-exr tmp/1/main.ηc.ξc.js main.η tmp/2; exr main.ξc.js tmp/2/main.ηc.ξ .
-exr tmp/2/main.ηc.ξc.js main.η tmp/3; exr main.ξc.js tmp/3/main.ηc.ξ .
+exr       η.ηc.ξc.js η.η tmp  ; exr ξ.ξc.js   tmp/η.ηc.ξ .
+exr   tmp/η.ηc.ξc.js η.η tmp/1; exr ξ.ξc.js tmp/1/η.ηc.ξ .
+exr tmp/1/η.ηc.ξc.js η.η tmp/2; exr ξ.ξc.js tmp/2/η.ηc.ξ .
+exr tmp/2/η.ηc.ξc.js η.η tmp/3; exr ξ.ξc.js tmp/3/η.ηc.ξ .
 if [ $? = 0 ]; then
 
 echo '--- success η ---'
-cp main.ηc.ξc.js main.ηc.ξc.js.bak
-cp tmp/3/main.ηc.ξc.js main.ηc.ξc.js
+cp η.ηc.ξc.js η.ηc.ξc.js.bak
+cp tmp/3/η.ηc.ξc.js η.ηc.ξc.js
 
-exr       main.φc.ηc.ξc.js main.φ tmp  ; exr main.ηc.ξc.js   tmp/main.φc.η .; exr main.ξc.js   tmp/main.φc.ηc.ξ .
-exr   tmp/main.φc.ηc.ξc.js main.φ tmp/1; exr main.ηc.ξc.js tmp/1/main.φc.η .; exr main.ξc.js tmp/1/main.φc.ηc.ξ .
-exr tmp/1/main.φc.ηc.ξc.js main.φ tmp/2; exr main.ηc.ξc.js tmp/2/main.φc.η .; exr main.ξc.js tmp/2/main.φc.ηc.ξ .
-exr tmp/2/main.φc.ηc.ξc.js main.φ tmp/3; exr main.ηc.ξc.js tmp/3/main.φc.η .; exr main.ξc.js tmp/3/main.φc.ηc.ξ .
+exr       φ.φc.ηc.ξc.js φ.φ tmp  ; exr η.ηc.ξc.js   tmp/φ.φc.η .; exr ξ.ξc.js   tmp/φ.φc.ηc.ξ .
+exr   tmp/φ.φc.ηc.ξc.js φ.φ tmp/1; exr η.ηc.ξc.js tmp/1/φ.φc.η .; exr ξ.ξc.js tmp/1/φ.φc.ηc.ξ .
+exr tmp/1/φ.φc.ηc.ξc.js φ.φ tmp/2; exr η.ηc.ξc.js tmp/2/φ.φc.η .; exr ξ.ξc.js tmp/2/φ.φc.ηc.ξ .
+exr tmp/2/φ.φc.ηc.ξc.js φ.φ tmp/3; exr η.ηc.ξc.js tmp/3/φ.φc.η .; exr ξ.ξc.js tmp/3/φ.φc.ηc.ξ .
 if [ $? = 0 ]; then
 
 echo '--- success φ ---'
-cp main.φc.ηc.ξc.js main.φc.ηc.ξc.js.bak
-cp tmp/3/main.φc.ηc.ξc.js main.φc.ηc.ξc.js
+cp φ.φc.ηc.ξc.js φ.φc.ηc.ξc.js.bak
+cp tmp/3/φ.φc.ηc.ξc.js φ.φc.ηc.ξc.js
 
 fi
 fi
