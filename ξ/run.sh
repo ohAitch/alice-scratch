a3d9474ab0004a917447163d5aff2f7ca6fc0fab
Alice Monday <alice0meta@gmail.com>
2014-06-23 19:23:43 -0700

    and now, macros with η!

--- ξ/run.sh
+++ ξ/run.sh
@@ -8,7 +8,20 @@ exr tmp/main.ξc.js main.ξ tmp/1
 exr tmp/1/main.ξc.js main.ξ tmp/2
 exr tmp/2/main.ξc.js main.ξ tmp/3
 if [ $? = 0 ]; then
-	echo '--- success ---'
-	cp main.ξc.js main.ξc.js.bak
-	cp tmp/3/main.ξc.js main.ξc.js
+
+echo '--- success ξ ---'
+cp main.ξc.js main.ξc.js.bak
+cp tmp/3/main.ξc.js main.ξc.js
+
+exr main.ηc.ξc.js       main.η tmp  ; exr main.ξc.js tmp/main.ηc.ξ .
+exr tmp/main.ηc.ξc.js   main.η tmp/1; exr main.ξc.js tmp/1/main.ηc.ξ .
+exr tmp/1/main.ηc.ξc.js main.η tmp/2; exr main.ξc.js tmp/2/main.ηc.ξ .
+exr tmp/2/main.ηc.ξc.js main.η tmp/3; exr main.ξc.js tmp/3/main.ηc.ξ .
+if [ $? = 0 ]; then
+
+echo '--- success η ---'
+cp main.ηc.ξc.js main.ηc.ξc.js.bak
+cp tmp/3/main.ηc.ξc.js main.ηc.ξc.js
+
+fi
 fi
