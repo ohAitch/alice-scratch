85878b5ed738bb9de1fa60d277f2955bfaff62e7
Alice Monday <alice0meta@gmail.com>
2014-06-27 14:34:15 -0700

    ξ now parenthesizes things properly

--- ξ/η.js
+++ ξ/η.js
@@ -5,9 +5,9 @@ var macros = {".":function(o,m){return([{s:"η/member"},o,m])},
 			"η/member":function(o,m){return([{s:"ξ/member_val"},o,m])},
 			"η/+":function(a,b){return([{s:"ξ/JS_+"},a,b])},
 			"η/~":function(a,b){return([{s:"ξ/JS_+"},a,b])}}
-var macro_expand = function(v){if (v instanceof Array) {while (macros[v[0]? v[0]["s"] :
-		undefined]) v = macros[v[0]["s"]]["apply"](null,v["slice"](1));
+var macro_expand = function(v){if ((v instanceof Array)) {while (macros[(v[0]? v[0]["s"] :
+		undefined)]) (v = macros[v[0]["s"]]["apply"](null,v["slice"](1)));
 	return(v["map"](macro_expand))}
 	else return(v)}
-var η_f = function(in_,out){fs["writeFileSync"](out+"/"+in_["slice"](0,Number("-2"))+".ξ",JSON["stringify"](macro_expand(JSON["parse"](fs["readFileSync"](in_)+" "))["slice"](1),null,"	"))}
+var η_f = function(in_,out){fs["writeFileSync"]((((out+"/")+in_["slice"](0,Number("-2")))+".ξ"),JSON["stringify"](macro_expand(JSON["parse"]((fs["readFileSync"](in_)+" ")))["slice"](1),null,"	"))}
 η_f(process["argv"][2],process["argv"][3])
