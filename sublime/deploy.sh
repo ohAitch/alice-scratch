db268bf7d23859343ad615eb8bb688089c871162
Alice Monday <alice0meta@gmail.com>
2014-06-27 13:50:35 -0700

    maint

--- sublime/deploy.sh
+++ sublime/deploy.sh
@@ -1,4 +1,17 @@
+Pack=~/"Library/Application Support/Sublime Text 3/Packages"
+
 generate.js
-rm -R ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
-rm -R ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Alpha
-cp -R Packages/ ~/Library/Application\ Support/Sublime\ Text\ 3/Packages
+
+mkdir -p tmp/User
+cp -R "Packages/User/Package Control.ca-bundle" tmp/User
+cp -R "Packages/User/Package Control.ca-list"   tmp/User
+cp -R "Packages/User/Package Control.last-run"  tmp/User
+cp -R "Packages/User/Package Control.cache"     tmp/User
+
+rm -R "$Pack/Alpha"
+rm -R "$Pack/User"
+
+cp -R Packages/ "$Pack"
+
+cp -R tmp/User "$Pack/User"
+rm -R tmp
--- sublime/fetch.sh
+++ sublime/fetch.sh
@@ -1,4 +1,11 @@
-rm -R Packages
-mkdir Packages
-cp -R ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Alpha Packages/Alpha
-cp -R ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User Packages/User
+Pack=~/"Library/Application Support/Sublime Text 3/Packages"
+
+rm -R Packages; mkdir Packages
+
+cp -R "$Pack/Alpha" "Packages/Alpha"
+cp -R "$Pack/User"  "Packages/User"
+
+rm -R "Packages/User/Package Control.ca-bundle"
+rm -R "Packages/User/Package Control.ca-list"
+rm -R "Packages/User/Package Control.last-run"
+rm -R "Packages/User/Package Control.cache"
