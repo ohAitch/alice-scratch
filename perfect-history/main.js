e30229c8ecb9df121d6d184c502d0042e4dbc510
Alice Monday <alice0meta@gmail.com>
2014-06-30 20:22:43 -0700

    *much* better.

--- perfect-history/main.js
+++ perfect-history/main.js
@@ -1,21 +1,7 @@
-#!/usr/bin/env node
+#!/usr/bin/env ζ core
 
-var fs = require('fs')
-var path = require('path')
-var chokidar = require('chokidar')
-var m = require('moment')
-
-var print = console.log.bind(console)
-
-var watch = process.argv[2]
-
-var out = function(fl,msg){return watch+'/.history/'+m.utc().toISOString()+' '+msg+' '+path.basename(fl)}
-
-var change = function(fl){fs.createReadStream(fl).pipe(fs.createWriteStream(out(fl,'+')))}
-var del = function(fl){fs.writeFileSync(out(fl,'-'),'')}
-
-chokidar.watch(watch, {persistent: true, ignoreInitial: true})
-	.on('add',   function(fl){if (!fl.match(/.history/)) {print('add   ',m.utc().toISOString(),path.basename(fl)); change(fl)}})
-	.on('change',function(fl){if (!fl.match(/.history/)) {print('change',m.utc().toISOString(),path.basename(fl)); change(fl)}})
-	.on('unlink',function(fl){if (!fl.match(/.history/)) {print('unlink',m.utc().toISOString(),path.basename(fl)); del(   fl)}})
-	.on('error',function(e){print('chokidar error:',e)})
+chokidar.watch(argv._[0],{persistent:true, ignoreInitial:true}).on('all', function(ev,fl){if ({add:1,change:1,unlink:1}[ev] && !fl.match(/\/\.history\//)) {
+	var now = moment.utc().toISOString(); var base = path.basename(fl)
+	print(pad(ev,' '.repeat(6)),now,base)
+	fs(argv._[0]+'/.history/'+now+' '+(ev==='unlink'?'-':'+')+' '+base).$ = ev==='unlink'? '' : fs(fl)
+}})
