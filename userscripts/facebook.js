db1c9e2d17f131ab4cc2a6935ae75cb4039b435c
Alice Monday <alice0meta@gmail.com>
2014-07-01 23:02:32 -0700

    these are silly

--- userscripts/facebook.js
+++
@@ -1,8 +0,0 @@
-// ==UserScript==
-// @name       facebook tweaks
-// @namespace  alice0meta.facebook
-// @version    0.1
-// @match      http*://*.facebook.com/*
-// ==/UserScript==
-
-document.getElementById('rightCol').remove()
