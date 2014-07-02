89783c6e1ab18821466471e39c76e2eec8fdd52b
Alice Monday <alice0meta@gmail.com>
2014-07-01 23:21:28 -0700

    obsessive perfection

--- sublime/generate.js
+++ sublime/generate.js
@@ -1,6 +1,9 @@
 #!/usr/bin/env ζ₀ core
 
-;(fs('snippets')+'').split('\n\n').map(function(v,i){
-	v = v.split('\n')
-	fs('Packages/User/'+i+'.sublime-snippet').$ = '<snippet><content><![CDATA[\n'+v.slice(1).join('\n')+'\n'+']]></content><tabTrigger>'+v[0]+'</tabTrigger></snippet>'
-	})
+fs.SUB_BYs = function(b){return this.split(b)}
+var SUB_Fs = function(s,f){return s.slice(f).slice(1).join(f)}
+var SUB_Ts = function(s,t){return s.slice(t)[0]}
+
+fs('snippets').SUB_BYs('\n\n').map(function(v,i){
+	fs('Packages/User/'+i+'.sublime-snippet').$ = '<snippet><content><![CDATA['+SUB_Fs(v,'\n')+'\n'+']]></content><tabTrigger>'+SUB_Ts(v,'\n')+'</tabTrigger></snippet>'
+})
