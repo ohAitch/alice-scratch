6ccf04d954c5434d1914384381b16da0eca8cd4f
Alice Monday <alice0meta@gmail.com>
2014-07-02 07:30:00 -0700

    done: draft of "output js at correct positions"

--- ζ₁/run.sh
+++ ζ₁/run.sh
@@ -2,11 +2,9 @@
 eval $mydir
 
 rm -r t; mkdir t; mkdir t/1; mkdir t/2; mkdir t/3; mkdir t/bin
-
-bin/ζ₁c *.ζ₁ t/1; ex t/1
-t/1/ζ₁c *.ζ₁ t/2; ex t/2
-t/2/ζ₁c *.ζ₁ t/3; ex t/3
-t/3/ζ₁c *.ζ₁ t/bin; ex t/bin
-if [ $? != 0 ]; then exit; fi
+        bin/ζ₁c *.ζ₁ t/1  ; if [ $? != 0 ]; then echo '--- 0 failed ---'; exit; fi
+ex t/1; t/1/ζ₁c *.ζ₁ t/2  ; if [ $? != 0 ]; then echo '--- 1 failed ---'; exit; fi
+ex t/2; t/2/ζ₁c *.ζ₁ t/3  ; if [ $? != 0 ]; then echo '--- 2 failed ---'; exit; fi
+ex t/3; t/3/ζ₁c *.ζ₁ t/bin; if [ $? != 0 ]; then echo '--- 3 failed ---'; exit; fi
 echo '--- success ---'
-cp -r t/bin .
+ex t/bin; cp -r t/bin .
