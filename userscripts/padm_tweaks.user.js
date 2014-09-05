fee9acba328f4dd07432222f86a8d3dfb0660bfd
Alice Monday <alice0meta@gmail.com>
2014-09-05 14:53:08 -0700

    doh

--- userscripts/padm_tweaks.user.js
+++ userscripts/padm_tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        padm tweaks
 // @namespace   comely-naiad
-// @version     1.0.3
+// @version     1.0.4
 // @downloadURL https://github.com/alice0meta/scratch/raw/master/userscripts/padm_tweaks.user.js
 // @match       http://*.padm.us/*
 // @require     https://code.jquery.com/jquery-latest.js
@@ -13,7 +13,7 @@ $.on_view = function(f){document.addEventListener('visibilitychange',function(){
 String.prototype.repeat = function(v){return new Array(v+1).join(this)}
 
 $.css(
-	'.floaty_btn {font-size:large; font-family:monospace; background-color:#333; padding:3px; position:fixed; left:10%; top:6px; z-index:10; border-radius:5px; text-decoration:none; color:#fff; line-height:normal;}'+
+	'.floaty_btn {font-size:large; font-family:monospace; background-color:#333; padding:3px; position:fixed; left:143px; top:6px; z-index:10; border-radius:5px; text-decoration:none; color:#fff; line-height:normal;}'+
 	'.floaty_btn:hover {text-decoration:underline;}'+
 	'')
 
