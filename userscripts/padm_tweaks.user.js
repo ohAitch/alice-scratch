266ff70fe5c00c0851e2ff28296438f127f3d71d
Alice Monday <alice0meta@gmail.com>
2014-09-05 16:46:18 -0700

    bosh those silly useMonospaceFont urls

--- userscripts/padm_tweaks.user.js
+++ userscripts/padm_tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        padm tweaks
 // @namespace   comely-naiad
-// @version     1.0.4
+// @version     1.0.5
 // @downloadURL https://github.com/alice0meta/scratch/raw/master/userscripts/padm_tweaks.user.js
 // @match       http://*.padm.us/*
 // @require     https://code.jquery.com/jquery-latest.js
@@ -11,19 +11,23 @@
 $.css = function(v){$('head').append('<style>'+v+'</style>')}
 $.on_view = function(f){document.addEventListener('visibilitychange',function(){if (!document.hidden) f()})}
 String.prototype.repeat = function(v){return new Array(v+1).join(this)}
+var print = console.log.bind(console)
+var poll = function(f,cb){var t; if (t=f()) cb(null,t); else setTimeout(poll,10,f,cb)}
 
 $.css(
 	'.floaty_btn {font-size:large; font-family:monospace; background-color:#333; padding:3px; position:fixed; left:143px; top:6px; z-index:10; border-radius:5px; text-decoration:none; color:#fff; line-height:normal;}'+
 	'.floaty_btn:hover {text-decoration:underline;}'+
 	'')
 
+var t
 if (location.href.match(/expost.padm.us/)) {
-	$('body').append('<a class="floaty_btn" href="'+location.href.replace('expost.padm.us','padm.us')+'?useMonospaceFont=true">edit</a>')
+	$('body').append('<a class="floaty_btn" href="'+location.href.replace('expost.padm.us','padm.us')+'">edit</a>')
 } else {
-	if (!location.href.match(/useMonospaceFont/)) window.location = location.href+'?useMonospaceFont=true'
+	if (t=location.href.match(/^(.+)\?useMonospaceFont=true$/)) window.location = t[1]
 	else {
 		$.on_view(function(){if ($('#connectionbox').is(":visible")) location.reload()})
-		$('body').append('<a class="floaty_btn" href="'+location.href.replace('padm.us','expost.padm.us').replace('?useMonospaceFont=true','')+'">expost</a>')
+		$('body').append('<a class="floaty_btn" href="'+location.href.replace('padm.us','expost.padm.us')+'">expost</a>')
 		$('.acl-write.separator').first().after('<li class="acl-write separator"></li>'.repeat(8))
+		_.range(50).map(function(v){setTimeout(function(){unsafeWindow.pad.changeViewOption('useMonospaceFont',true)},(0.8+v*0.2)*1000)})
 	}
 }
