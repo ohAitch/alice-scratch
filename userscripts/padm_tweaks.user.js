2db3a7a3ea2192ef6d3c65936dc98068784a82bc
Alice Monday <alice0meta@gmail.com>
2014-09-05 14:49:57 -0700

    saner button position

--- userscripts/padm_tweaks.user.js
+++ userscripts/padm_tweaks.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        padm tweaks
 // @namespace   comely-naiad
-// @version     1.0.2
+// @version     1.0.3
 // @downloadURL https://github.com/alice0meta/scratch/raw/master/userscripts/padm_tweaks.user.js
 // @match       http://*.padm.us/*
 // @require     https://code.jquery.com/jquery-latest.js
@@ -10,15 +10,20 @@
 
 $.css = function(v){$('head').append('<style>'+v+'</style>')}
 $.on_view = function(f){document.addEventListener('visibilitychange',function(){if (!document.hidden) f()})}
+String.prototype.repeat = function(v){return new Array(v+1).join(this)}
 
-$.css('.floaty_button {font-size:large; font-family:monospace; background-color:#333; padding:3px; position:fixed; top:6px; border-radius:5px; text-decoration:none; color:#fff}')
+$.css(
+	'.floaty_btn {font-size:large; font-family:monospace; background-color:#333; padding:3px; position:fixed; left:10%; top:6px; z-index:10; border-radius:5px; text-decoration:none; color:#fff; line-height:normal;}'+
+	'.floaty_btn:hover {text-decoration:underline;}'+
+	'')
 
 if (location.href.match(/expost.padm.us/)) {
-	$('body').append('<a class="floaty_button" style="left:10%;" href="'+location.href.replace('expost.padm.us','padm.us')+'?useMonospaceFont=true">edit</a>')
+	$('body').append('<a class="floaty_btn" href="'+location.href.replace('expost.padm.us','padm.us')+'?useMonospaceFont=true">edit</a>')
 } else {
 	if (!location.href.match(/useMonospaceFont/)) window.location = location.href+'?useMonospaceFont=true'
 	else {
 		$.on_view(function(){if ($('#connectionbox').is(":visible")) location.reload()})
-		$('body').append('<a class="floaty_button" style="left:30%;" href="'+location.href.replace('padm.us','expost.padm.us').replace('?useMonospaceFont=true','')+'">expost</a>')
+		$('body').append('<a class="floaty_btn" href="'+location.href.replace('padm.us','expost.padm.us').replace('?useMonospaceFont=true','')+'">expost</a>')
+		$('.acl-write.separator').first().after('<li class="acl-write separator"></li>'.repeat(8))
 	}
 }
