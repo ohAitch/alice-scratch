aa9fb1d3f5f0d260d4f45f70ea1b0db7a22ee586
Alice Monday <alice0meta@gmail.com>
2014-09-04 23:33:23 -0700

    now with nice links

--- userscripts/padm_monospace.user.js
+++ userscripts/padm_monospace.user.js
@@ -1,8 +1,18 @@
 // ==UserScript==
-// @name        padm monospace
+// @name        padm tweaks
 // @namespace   comely-naiad
 // @version     1.0.0
-// @match       http://padm.us/*
+// @match       http://*.padm.us/*
+// @require     https://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-if (!location.href.match(/useMonospaceFont/)) window.location = location.href+'?useMonospaceFont=true'
+$.css = function(v){$('head').append('<style>'+v+'</style>')}
+
+$.css('.floaty_button {font-size:large; font-family:monospace; background-color:#333; padding:3px; position:fixed; top:6px; border-radius:5px; text-decoration:none; color:#fff}')
+
+if (!location.href.match(/expost.padm.us/)) {
+	if (!location.href.match(/useMonospaceFont/)) window.location = location.href+'?useMonospaceFont=true'
+	$('body').append('<a class="floaty_button" style="left:30%;" href="'+location.href.replace('padm.us','expost.padm.us').replace('?useMonospaceFont=true','')+'">expost</a>')
+} else {
+	$('body').append('<a class="floaty_button" style="left:10%;" href="'+location.href.replace('expost.padm.us','padm.us')+'?useMonospaceFont=true">edit</a>')
+}
