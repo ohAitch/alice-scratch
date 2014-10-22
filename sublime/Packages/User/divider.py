676b9dd6118c0ea1217857438db63eaae62448c2
Alice Monday <alice0meta@gmail.com>
2014-10-22 13:06:07 -0700

    refactor

--- sublime/Packages/User/divider.py
+++ sublime/Packages/User/divider.py
@@ -3,66 +3,27 @@ import re
 
 # oh god
 # this file is mostly hacked together when we needed the functionality but also needed to get back to something else
-
-def make_divider(head,mid,s,length):
-	a = head
-	b = head[::-1]+' '+s+' '+head
-	c = head[::-1]
-	t = length-len(a)-len(b)-len(c)
-	return a + (mid*((t+1)//2)) + b + (mid*(t//2)) + c
+# i guess it's a bit better now though :)
 
 class DividerCommand(sublime_plugin.TextCommand):
-	def run(self,edit,style,length):
-		fl = self.view.file_name()
-		hashs = ['.rb','.py']
-		slashs = ['.js']
-		cstyle = 'dash' if fl is None else 'hash' if any([fl.endswith(v) for v in hashs]) else 'slash' if any([fl.endswith(v) for v in slashs]) else 'dash'
-		# ext = re.sub("^.*\.([^.]+)$","\\1",self.view.file_name())
+	def run(self,edit,length):
+		e_table_ = {'rb':'#', 'py':'#', 'sh':'#', 'js':'/'}
+		def e_table(v): return e_table_[v] if v in e_table_ else '-'
+		s_table = {
+			'#': ["^#+.*#+$","^#+\s*(.+?)\s*#+$",['',''],'#'],
+			'-': ["^-+.*-+$","^-+\s*(.+?)\s*-+$",['',''],'-'],
+			'/': ["^//=+-+.*-+=+//$","^//=+-+=+//\s*(.+?)\s*//=+-+=+//$",['//===','===//'],'-']
+			}
+		ext = '' if self.view.file_name() is None else re.sub("^.*?(?:\.([^.]*))?$","\\1",self.view.file_name())
+		data = s_table[e_table(ext)]
 		for region in reversed([s for s in self.view.sel()]):
 			line = self.view.line(region)
 			s = self.view.substr(line)
-			if style == "big":
-				# x = re.match("^(---+)\s*(.+?)\s*$",s)
-				x = re.match("^(#+)\s*([^#\n]+?)\s*$",s)
-				if x:
-					# t = re.match("^(---+)\s*(.+?)\s*\\1$",s)
-					t = re.match("^(#+)\s*(.+?)\s*\\1$",s)
-					if t:
-						return
-						#begin =     ('-'*((len(s)-len(t.group(2))-2 + 1)//2))+' '
-						#end   = ' '+('-'*((len(s)-len(t.group(2))-2    )//2))
-						#self.view.replace(edit,line,begin+t.group(2)+end)
-					else:
-						# begin =     ('-'*((len(x.group(1))-len(x.group(2))-2 + 1)//2))+' '
-						# end   = ' '+('-'*((len(x.group(1))-len(x.group(2))-2    )//2))
-						begin =     ('#'*((len(x.group(1))-len(x.group(2))-2 + 1)//2))+' '
-						end   = ' '+('#'*((len(x.group(1))-len(x.group(2))-2    )//2))
-						self.view.replace(edit,line,begin+x.group(2)+end)
-				else:
-					if cstyle == 'hash':
-						if re.match("^#+.*#+$",s):
-							s = re.match("^#+\s*(.+?)\s*#+$",s).group(1)
-						else:
-							s = s.strip()
-						begin =     ('#'*((length-len(s)-2 + 1)//2))+' '
-						end   = ' '+('#'*((length-len(s)-2    )//2))
-						self.view.replace(edit,line,begin+s+end)
-					elif cstyle == 'slash':
-						if re.match("^//=+-+.*-+=+//$",s):
-							s = re.match("^//=+-+=+//\s*(.+?)\s*//=+-+=+//$",s).group(1)
-						else:
-							s = s.strip()
-						begin =     '//==='+('-'*((length-len(s)-2 + 1)//2-10))+'===//'+' '
-						end   = ' '+'//==='+('-'*((length-len(s)-2    )//2-10))+'===//'
-						self.view.replace(edit,line,begin+s+end)
-					elif cstyle == 'dash':
-						if re.match("^-+.*-+$",s):
-							s = re.match("^-+\s*(.+?)\s*-+$",s).group(1)
-						else:
-							s = s.strip()
-						begin =     ('-'*((length-len(s)-2 + 1)//2))+' '
-						end   = ' '+('-'*((length-len(s)-2    )//2))
-						self.view.replace(edit,line,begin+s+end)
-			elif style == "small":
-				t = re.match("^---+\s*(.+?)\s*---+$",s)
-				self.view.replace(edit,line,make_divider('','-',t.group(1) if t else s.strip(),length))
+			if re.match(data[0],s):
+				s = re.match(data[1],s).group(1)
+			else:
+				s = s.strip()
+			t = sum([len(v) for v in data[2]])
+			begin =     data[2][0]      +(data[3]*((length-len(s)-2 + 1)//2 - t))+data[2][1]      +' '
+			end   = ' '+data[2][1][::-1]+(data[3]*((length-len(s)-2    )//2 - t))+data[2][0][::-1]
+			self.view.replace(edit,line,begin+s+end)
