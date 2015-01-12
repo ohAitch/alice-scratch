f7f59b7cb8aaf18f0eac374fdba604f25912aac0
Alice Monday <alice0meta@gmail.com>
2015-01-11 18:51:29 -0800

    minor

--- userscripts/attention_thedailywtf.user.js
+++ userscripts/attention_thedailywtf.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name        attention: thedailywtf
 // @namespace   comely-naiad
-// @version     1.0.1
+// @version     1.0.2
 // @downloadURL https://github.com/alice0meta/scratch/raw/master/userscripts/attention_thedailywtf.user.js
 // @match       http://thedailywtf.com/*
 // @require     https://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 $('aside').slice(1).remove()
+$('.tales').remove()
