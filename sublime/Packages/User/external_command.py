c8f41b7dbd849044773bf11085ad6e27a4e1ed18
Alice Monday <alice0meta@gmail.com>
2015-03-03 09:56:52 -0800

    let sublime be commandable externally

---
+++ sublime/Packages/User/external_command.py
@@ -0,0 +1,33 @@
+import sublime, sublime_plugin
+import threading
+import http.server
+import json
+
+class Handler(http.server.BaseHTTPRequestHandler):
+	def do_PUT(self):
+		body = self.rfile.read(int(self.headers.get('content-length')))
+		locals = {}
+		exec(body,None,locals)
+		self.send_response(200)
+		if 'r' in locals:
+			t = json.dumps(locals['r'])
+			self.wfile.write(bytes((t if len(t) >= 6 else t+'    ')+'\n','UTF-8'))
+	def log_request(self,code='-',size='-'):
+		pass
+t = http.server.HTTPServer(('127.0.0.1', 34289), Handler)
+def serve():
+	try:
+		t.serve_forever()
+	except:
+		pass
+threading.Thread(target=serve, daemon=True).start()
+print("[EXTERNAL COMMAND] server started")
+
+def plugin_unloaded():
+	print('[EXTERNAL COMMAND] server shutting down')
+	t.socket.close()
+
+class Listener(sublime_plugin.EventListener):
+	def on_activated(self,view_):
+		global view
+		view = view_
