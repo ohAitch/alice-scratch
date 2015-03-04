73f25f90b14e9c31a9d479033665f1fa80c0909e
Alice Monday <alice0meta@gmail.com>
2015-03-03 17:54:03 -0800

    finish up external_command.py

--- sublime/Packages/User/external_command.py
+++ sublime/Packages/User/external_command.py
@@ -12,22 +12,21 @@ class Handler(http.server.BaseHTTPRequestHandler):
 		if 'r' in locals:
 			t = json.dumps(locals['r'])
 			self.wfile.write(bytes((t if len(t) >= 6 else t+'    ')+'\n','UTF-8'))
+		else:
+			self.wfile.write(bytes('null  ','UTF-8'))
 	def log_request(self,code='-',size='-'):
 		pass
 t = http.server.HTTPServer(('127.0.0.1', 34289), Handler)
 def serve():
-	try:
-		t.serve_forever()
-	except:
-		pass
+	try: t.serve_forever()
+	except: pass
 threading.Thread(target=serve, daemon=True).start()
 print("[EXTERNAL COMMAND] server started")
 
-def plugin_unloaded():
-	print('[EXTERNAL COMMAND] server shutting down')
-	t.socket.close()
+def plugin_unloaded(): print('[EXTERNAL COMMAND] server shutting down'); t.socket.close()
 
 class Listener(sublime_plugin.EventListener):
-	def on_activated(self,view_):
-		global view
-		view = view_
+	def on_activated(self,view_): global view; view = view_
+
+class ExecEditCommand(sublime_plugin.TextCommand):
+	def run(self,edit,v): view = self.view; exec(v)
