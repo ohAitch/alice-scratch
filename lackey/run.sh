f526b0c9ee5761eaffe4ab1e5fb0916093dbe61b
Alice Monday <alice0meta@gmail.com>
2015-03-22 22:03:43 -0700

    lackey upgrade to ζ₂ and some modern design

--- lackey/run.sh
+++ lackey/run.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-. ~/.bashrc
-eval $mydir
-
-nvm use 0.10
+ζr() { ζ₂ -c "$1" .; exr "${1/.ζ₂/.js}" "${@:2}"; rm "${1/.ζ₂/.js}"; }
 
 cp lackey.icns ~/"Library/Keyboard Layouts/"
-make_keylayout.js
+ζr make_keylayout.ζ₂ ~/"Library/Keyboard Layouts/lackey.keylayout"
 
-make_karab.js alice.lackey > ~/"Library/Application Support/Karabiner/private.xml"
+ζr make_karab.ζ₂ alice.lackey ~/"Library/Application Support/Karabiner/private.xml"
 /Applications/Karabiner.app/Contents/Library/bin/karabiner reloadxml
 /Applications/Karabiner.app/Contents/Library/bin/karabiner enable lackey
