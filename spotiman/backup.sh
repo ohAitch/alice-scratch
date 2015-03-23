7516ca7f086aa088fa451037fe5beac3b031a52f
Alice Monday <alice0meta@gmail.com>
2015-03-22 22:03:15 -0700

    spotiman upgrade to node v0.12

--- spotiman/backup.sh
+++ spotiman/backup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-. ~/.bashrc
+date_i() { date -u +"%Y-%m-%dT%H:%M:%SZ"; }
+ζr() { ζ₂ -c "$1" .; exr "${1/.ζ₂/.js}" "${@:2}"; rm "${1/.ζ₂/.js}"; }
 
 out=~/ali/history/auto/spotify/$(date_i).json
-./main.sh backup > "$out"
+ζr main.ζ₂ backup > "$out"
 echo "$out"
--- spotiman/main.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-. ~/.bashrc
-
-nvm use 0.10 &>/dev/null
-
-ζ₂ -c main.ζ₂ .
-exr ./main.js "$@"
-rm main.js
