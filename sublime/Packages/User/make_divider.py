3ec9a6fb1d3858802479b3de6ffa1998d406686c
Alice Monday <alice0meta@gmail.com>
2015-08-18 14:10:41 -0700

    respect tabs at the beginning of a line

--- sublime/Packages/User/make_divider.py
+++ sublime/Packages/User/make_divider.py
@@ -23,6 +23,7 @@ class MakeDividerCommand(sublime_plugin.TextCommand):
 		for i,region in [v for v in enumerate(view.sel())][::-1]:
 			line = view.line(region)
 			s = view.substr(line)
+			t = re.match(r'^(\t*)(.*)',s); tabs = t.group(1); s = t.group(2)
 			if re.match(test,s):
 				s = re.match(match,s).group(1)
 			else:
@@ -30,15 +31,15 @@ class MakeDividerCommand(sublime_plugin.TextCommand):
 			def len_a(v): return sum([len(v) for v in v])
 			if s == '':
 				r = [ends,'',ends[::-1]]
-				len_ = length - len_a(r)
+				len_ = length - len(tabs)*2 - len_a(r)
 				r[1] = fill * len_
 			else:
 				r = [ends,'',' ',s,' ','',ends[::-1]]
-				len_ = length - len_a(r)
+				len_ = length - len(tabs)*2 - len_a(r)
 				while len_ < 6: len_ += 10
 				r[1] = fill * ((len_+1)//2)
 				r[5] = fill * (len_//2)
 			q = region == line and region.empty()
 			if q: view.sel().subtract(region)
-			view.replace(edit,line,''.join(r))
+			view.replace(edit,line,tabs+''.join(r))
 			if q: view.sel().add(sublime.Region(view.line(region).end()))
