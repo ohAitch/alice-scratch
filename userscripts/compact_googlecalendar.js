b44a2b1192dfb60e1322b876f407e2a2d01ee598
Alice Monday <alice0meta@gmail.com>
2015-08-18 14:10:21 -0700

    update userscripts

--- userscripts/compact_googlecalendar.js
+++ userscripts/compact_googlecalendar.js
@@ -1,10 +1,15 @@
 // ==UserScript==
 // @name        compact: google calendar
 // @namespace   comely-naiad
-// @version     1.0.0
+// @version     1.0.1
 // @downloadURL https://github.com/alice0meta/scratch/raw/master/userscripts/compact_googlecalendar.user.js
 // @match       http*://www.google.com/calendar/render*
 // @grant       none
 // ==/UserScript==
 
-document.getElementById('onegoogbar').remove()
+var t = document.getElementById("onegoogbar")
+while (t.firstChild) t.removeChild(t.firstChild)
+t.style.height = "30px"
+t.style.background_color = "#f1f1f1"
+
+;
