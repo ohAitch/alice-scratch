b1f1bb59f88c2ebe8d8a9c7d70f610dc725f367a
Alice Monday <alice0meta@gmail.com>
2015-09-10 19:24:02 -0700

    init

---
+++ userscripts/asoftmurmur.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        asoftmurmur
+// @namespace   comely-naiad
+// @version     1.0.0
+// @downloadURL https://github.com/alice0meta/scratch/raw/master/userscripts/asoftmurmur.user.js
+// @match       http*://*.asoftmurmur.com/*
+// @grant       none
+// ==/UserScript==
+
+$('#action-bar, #site-title, #intro-text, #gp-container, #about-link-container').remove()
