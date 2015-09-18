1a7db18f6f39cf1f9dfb9422d24febd08a745b94
Alice Monday <alice0meta@gmail.com>
2015-09-18 14:16:54 -0700

    combine make_divider.py, datetime_completion.py into text_gen.py

--- sublime/Packages/User/text_gen.py
+++ sublime/Packages/User/text_gen.py
@@ -1,5 +1,8 @@
 import sublime, sublime_plugin
 import re
+import time
+from datetime import datetime
+# from datetime import timedelta
 
 # i wanna styles of different boldness, like, ===== is bolder than -----, and i wanna switch between them iff i hit the divider key and the length doesn't change
 # and maybe if you hit the command again it should unmake the divider?
@@ -43,3 +46,25 @@ class MakeDividerCommand(sublime_plugin.TextCommand):
 			if q: view.sel().subtract(region)
 			view.replace(edit,line,tabs+''.join(r))
 			if q: view.sel().add(sublime.Region(view.line(region).end()))
+
+# maybe this really should do that reverse generation (aka: parsing) we tried having it do
+# maybe this should merge with our snippets file
+
+# def round_time(dt, round_to):
+# 	seconds = (dt - dt.min).seconds
+# 	rounding = (seconds+round_to/2) // round_to * round_to
+# 	return dt + timedelta(0,rounding-seconds,-dt.microsecond)
+
+class _(sublime_plugin.EventListener):
+	def on_query_completions(self, view, prefix, locations):
+		u = datetime.utcnow()
+		l = datetime.now()
+		return ([
+			['now', u.strftime('%Y-%m-%dT%H:%MZ')],
+			['now', u.strftime('%Y-%m-%dT%H:%M:%SZ')],
+			['now', u.strftime('%Y-%m-%dT%H:%M:%S.')+u.strftime('%f')[:3]+'Z'],
+			['lnow', l.strftime('%Y-%m-%d/%H:%M')],
+			['day', l.strftime('%Y-%m-%d')],
+			['week', l.strftime('%Y-W%W')],
+			['week', l.strftime('%Y-W%W-') + str((int(l.strftime('%w')) - 1) % 7 + 1)]
+		],sublime.INHIBIT_WORD_COMPLETIONS)
