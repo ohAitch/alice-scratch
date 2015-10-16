09c713b6876a41595057615b1f4d4e1abb403a1d
Alice Monday <alice0meta@gmail.com>
2015-10-15 23:06:47 -0700

    minor

--- sublime/Packages/User/build_dollar.py
+++ sublime/Packages/User/build_dollar.py
@@ -9,5 +9,6 @@ class BuildDollarCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
 		view = self.view
 		view.run_command("save")
-		t = "clear; cd "+bash_encode(os.path.dirname(view.file_name()))+"; echo $green$(this)$reset $; $; x"
+		# t = "clear; cd "+bash_encode(os.path.dirname(view.file_name()))+"; echo $green$(this)$reset ↩\\;x; ↩"
+		t = "clear; cd "+bash_encode(os.path.dirname(view.file_name()))+"; echo $green~${PWD:${#HOME}}$reset ↩\\; x; ↩; x" # wat
 		os.system("osascript -e "+bash_encode("tell application \"terminal\"")+" -e "+bash_encode("do script "+osa_encode(t))+" -e end\\ tell")
