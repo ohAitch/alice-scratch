b47f7c2de90fa83968c6b0ceaebc4d711aa7e7c2
Alice Monday <alice0meta@gmail.com>
2015-11-12 05:33:43 -0800

    lazy-load most everything that it's possible to

---
+++ ζ₂/bootstrap.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node --harmony
+
+var ζ2_compile = function(ι){
+	return ι.replace(/(?:λ\*?(?: λ)?(?=(?:[ \t][^\(=←]*)?\([^\)]*\)[ \t]*\{)|↩(?!\]) ?|([\w$ʰ-ʸˡ-ˣΑ-ΡΣ-ωᴬ-ᵛᵢ-ᵥᶜᶠᶻ⁰ⁱⁿₐ-ₓₕ-ₜℂℕℚℝℤⱼⱽ]+)(\s*)←(;?))(?!['"])/g,
+		function(ι,name,s,semi){
+			return {'λ':'function', 'λ λ':'function λ','λ*':'function*', 'λ* λ':'function* λ', '↩':'return ', '↩ ':'return '}[ι]
+				|| (semi===';'? 'var '+name+s+';' : 'var '+name+s+'=')})
+		.replace(/([^'"])@(?![-'"\w])/g,'$1this')}
+
+var t = function(ι){eval(ζ2_compile(require('fs').readFileSync(__dirname+'/'+ι+'.ζ₂')+''))}
+t('builtins')
+if (!module.parent) t('index')
