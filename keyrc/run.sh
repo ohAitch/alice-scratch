d88ea91649f2d7e98018db925c9f5f03338d09f6
Alice Monday <alice0meta@gmail.com>
2015-11-19 04:43:14 -0800

    move keylayout generation code into subfolder

--- keyrc/run.sh
+++ keyrc/run.sh
@@ -2,6 +2,5 @@
 -q(){ "$@" &>/dev/null; }
 ζr(){ -q pushd $(dirname "$1"); ζ₂ -c "$1" .; -q popd; chmod +x "${1/.ζ₂/.js}"; "${1/.ζ₂/.js}" "${@:2}"; E=$?; rm "${1/.ζ₂/.js}"; return $E; }
 
-cp lackey.icns ~/Library/Keyboard\ Layouts/ &&
-ζr make_keylayout.ζ₂ ~/Library/Keyboard\ Layouts/lackey.keylayout &&
-ζr make_karab.ζ₂ default.lackey ~/Library/Application\ Support/Karabiner/private.xml
+keylayout/index.ζ₂ &&
+ζr index.ζ₂ ~/.keyrc ~/Library/Application\ Support/Karabiner/private.xml
