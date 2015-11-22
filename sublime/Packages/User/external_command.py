d4c0b2e24fb6235495dd749b345eeabac59203c5
Alice Monday <alice0meta@gmail.com>
2015-11-22 01:35:05 -0800

    polished up sublime's eval api

--- sublime/Packages/User/external_command.py
+++ sublime/Packages/User/external_command.py
@@ -2,34 +2,42 @@ import sublime, sublime_plugin
 import threading
 import http.server
 import json
+import re # for code in execs
+
+PORT = 34289
 
 class Handler(http.server.BaseHTTPRequestHandler):
-	def do_PUT(self):
-		body = self.rfile.read(int(self.headers.get('content-length')))
-		locals = {}
-		exec(body,None,locals)
-		self.send_response(200)
-		if 'r' in locals:
-			t = json.dumps(locals['r'])
-			self.wfile.write(bytes((t if len(t) >= 6 else t+'    ')+'\n','UTF-8'))
+	def do_PUT(self_):
+		ι = self_.rfile.read(int(self_.headers.get('content-length'))).decode('utf-8')
+		ι = re.sub('(\n|;|$)',r' = r\1',ι[::-1],1)[::-1]
+		if re.search(r'view\.(insert|erase|replace)\(',ι):
+			global exec_edit_ι; global exec_edit_r
+			exec_edit_ι = re.sub(r'(\bview\.(insert|erase|replace)\()',r'\1edit,',ι)
+			g_view.run_command("exec_edit")
+			r = json.dumps(exec_edit_r)
 		else:
-			self.wfile.write(bytes('null  ','UTF-8'))
-	def log_request(self,code='-',size='-'):
+			r = {'self':g_self, 'view':g_view, 'window':g_window}
+			exec(ι,None,r)
+			r = json.dumps(r['r']) if 'r' in r else 'null'
+		self_.send_response(200)
+		self_.wfile.write(bytes(r+'    \n','UTF-8'))
+	def log_request(self_,code='-',size='-'):
 		pass
-t = http.server.HTTPServer(('127.0.0.1', 34289), Handler)
-def serve():
-	try: t.serve_forever()
+server = http.server.HTTPServer(('127.0.0.1',PORT), Handler)
+def t():
+	try: server.serve_forever()
 	except: pass
-threading.Thread(target=serve, daemon=True).start()
+threading.Thread(target=t, daemon=True).start()
 print("[EXTERNAL COMMAND] server started")
-
-def plugin_unloaded(): print('[EXTERNAL COMMAND] server shutting down'); t.socket.close()
+def plugin_unloaded(): print('[EXTERNAL COMMAND] server shutting down'); server.socket.close()
 
 class Listener(sublime_plugin.EventListener):
-	def on_activated(self,view_): global view; view = view_
+	def on_activated(self,view): global g_self; global g_view; g_self = self; g_view = view
+	def on_window_command(self,window,cmd,args): global g_window; g_window = window;
 
 class ExecEditCommand(sublime_plugin.TextCommand):
-	def run(self,edit,v): view = self.view; exec(v)
-
-# sublime_eval('view.run_command("exec_edit",{"v":"view.insert(edit,view.sel()[0].begin(),\\"\\\\n\\")"})')
-# setTimeout(λ(){ sublime_eval("import re\nt = view.substr(sublime.Region(0,view.size()))\nt = re.search(r'---+ percolation ---+[\\s\\S]*?---+ ready ---+[\\s\\S]*?(\\n)\\n*---+ queue ---+[\\s\\S]*?(\\n[ \\t]+)(.+)',t)\nview.run_command('exec_edit',{'v':'view.erase(edit,sublime.Region('+repr(t.start(2))+','+repr(t.end(3))+'))\\nview.insert(edit,'+repr(t.end(1))+','+repr(t.group(3)+'\\n')+')'})") },500)
+	def run(self,edit):
+		global exec_edit_r; exec_edit_r = None
+		r = {'self':self,'view':self.view,'edit':edit, 'window':g_window}
+		exec(exec_edit_ι,None,r)
+		if 'r' in r: exec_edit_r = r['r']
