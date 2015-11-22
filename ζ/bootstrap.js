a16cf4fe07754a95104442b192785529f77bd287
Alice Monday <alice0meta@gmail.com>
2015-11-22 01:36:13 -0800

    many things

--- ζ/bootstrap.js
+++ ζ/bootstrap.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node --harmony
 
-var ζ2_compile = function(ι){
-	return ι.replace(/(?:λ\*?(?: λ)?(?=(?:[ \t][^\(=←]*)?\([^\)]*\)[ \t]*\{)|↩(?!\]) ?|([\w$ʰ-ʸˡ-ˣΑ-ΡΣ-ωᴬ-ᵛᵢ-ᵥᶜᶠᶻ⁰ⁱⁿₐ-ₓₕ-ₜℂℕℚℝℤⱼⱽ]+)(\s*)←(;?))(?!['"])/g,
+var ζ_compile = function(ι){
+	return ι.replace(/(?:λ\*?(?: λ)?(?=(?:[ \t][^\(=←]*)?\([^\)]*\)[ \t]*\{)|↩(?!\]) ?|([\w$ʰ-ʸˡ-ˣΑ-ΡΣ-ωᴬ-ᵛᵢ-ᵥᶜᶠᶻ⁰ⁱⁿₐ-ₓₕ-ₜℂℕℚℝℤⱼⱽ]+)(\s*)←(;?))(?!['"])|⟩(?!['"]|\/g)/g,
 		function(ι,name,s,semi){
-			return {'λ':'function', 'λ λ':'function λ','λ*':'function*', 'λ* λ':'function* λ', '↩':'return ', '↩ ':'return '}[ι]
+			return {'λ':'function', 'λ λ':'function λ','λ*':'function*', 'λ* λ':'function* λ', '↩':'return ', '↩ ':'return ', '⟩':'.__special_pipe,'}[ι]
 				|| (semi===';'? 'var '+name+s+';' : 'var '+name+s+'=')})
 		.replace(/([^'"])@(?![-'"\w\/])/g,'$1this')}
 
-var t = function(ι){eval(ζ2_compile(require('fs').readFileSync(__dirname+'/'+ι+'.ζ₂')+''))}
+var t = function(ι){eval(ζ_compile(require('fs').readFileSync(__dirname+'/'+ι+'.ζ')+''))}
 t('builtins')
 if (!module.parent) t('index')
+
--- ζ/run.sh
+++ ζ/run.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
-npmi() { mkdir npm_inc_tmp; mv package.json npm_inc_tmp; cd npm_inc_tmp; npm version patch; mv package.json ..; cd ..; rmdir npm_inc_tmp; }
-
-# if versions are equal:
-# global.cmp_versions = λ(cb){
-# 	version_lt ← λ(a,b){a=a.split('.').map(i); b=b.split('.').map(i); ↩ a[0]<b[0] || (a[0]===b[0] && (a[1]<b[1] || (a[1]===b[1] && a[2]<b[2])))}
-# 	local ← JSON.parse(fs('package.json').$)
-# 	exec("curl -L '"+'https://raw.github.com/'+local.repository.url.match(/^https:\/\/github.com\/([\w-]+\/[\w-]+)\.git$/)[1]+'/master/package.json'+"'",λ(e,v){canon←;;
-# 		if (v!=='' && (canon = JSON.parse(v), version_lt(local.version,canon.version))) cb(null,local.version+' → '+canon.version)
-# 		else cb()
-# 	}) }
-# then increment patch by 1
-
-[[ $1 ]] || npmi
+[[ $(jq .version package.json) = $(jq .version /usr/local/lib/node_modules/$(jq -r .name package.json)/package.json) ]] && npm --no-git-tag-version version patch
+npm install . -g &&
 npm publish .
-sleep 0.1
-npm install zeta-two -g
