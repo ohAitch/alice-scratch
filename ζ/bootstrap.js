0bd5a94a9399ce18ec289d4dbfa894652b7150f7
Alice Monday <alice0meta@gmail.com>
2015-11-22 02:11:26 -0800

    adding ⟩ oh god why

--- ζ/bootstrap.js
+++ ζ/bootstrap.js
@@ -3,11 +3,10 @@
 var ζ_compile = function(ι){
 	return ι.replace(/(?:λ\*?(?: λ)?(?=(?:[ \t][^\(=←]*)?\([^\)]*\)[ \t]*\{)|↩(?!\]) ?|([\w$ʰ-ʸˡ-ˣΑ-ΡΣ-ωᴬ-ᵛᵢ-ᵥᶜᶠᶻ⁰ⁱⁿₐ-ₓₕ-ₜℂℕℚℝℤⱼⱽ]+)(\s*)←(;?))(?!['"])|⟩(?!['"]|\/g)/g,
 		function(ι,name,s,semi){
-			return {'λ':'function', 'λ λ':'function λ','λ*':'function*', 'λ* λ':'function* λ', '↩':'return ', '↩ ':'return ', '⟩':'.__special_pipe,'}[ι]
+			return {'λ':'function', 'λ λ':'function λ','λ*':'function*', 'λ* λ':'function* λ', '↩':'return ', '↩ ':'return ', '⟩':'.__special_pipe(),'}[ι]
 				|| (semi===';'? 'var '+name+s+';' : 'var '+name+s+'=')})
 		.replace(/([^'"])@(?![-'"\w\/])/g,'$1this')}
 
 var t = function(ι){eval(ζ_compile(require('fs').readFileSync(__dirname+'/'+ι+'.ζ')+''))}
 t('builtins')
 if (!module.parent) t('index')
-
