f9476d8b718bb434c046bb468c80f2e6f24173dd
Alice Monday <alice0meta@gmail.com>
2015-11-30 13:16:12 -0800

    nicer url

--- sublime/Packages/User/munge_stuff.py
+++ sublime/Packages/User/munge_stuff.py
@@ -9,8 +9,8 @@
 import sublime, sublime_plugin
 import os, subprocess, re, urllib, json
 
-URL = r'\b(?:https?://|file:/|mailto:)(?:[^\s“”\'"<>]*\([^\s“”\'"<>]*\))?(?:[^\s“”\'"<>]*[^\s“”\'"<>)\]}⟩?!,.:;])?'
-IS_URL = r'^(?:https?://|file:/|mailto:)'
+URL = r'\b(?:https?://|(?:file|mailto):)(?:[^\s“”"<>]*\([^\s“”"<>]*\))?(?:[^\s“”"<>]*[^\s“”"<>)\]}⟩?!,.:;])?'
+IS_URL = r'^(?:https?://|(?:file|mailto):)'
 
 def ζ(ι): return subprocess.check_output(['/usr/bin/env','/usr/local/bin/node','--harmony','/usr/local/bin/ζ','-e',ι]).decode('utf-8')
 
@@ -42,34 +42,25 @@ def echo_string_pipe_process__check_output(string,args):
 	# ignore stderr
 	return t[0].decode('utf-8')
 
-def open(ι,app=None,focus=True):
-	if not focus: subprocess.call(['bash','-ci','ack'])
+def open(ι,app=None,focus=True,view=None):
 	print("#OPEN",ι)
-	if ι is None and app is "Terminal": ι = os.getenv("HOME")
+	if not focus: subprocess.call(['bash','-ci','ack'])
 
-	#! wat
-	ι = re.sub(r'\\ ',' ',ι)
-	# # replace "\ " in filenames with " "
-	# #! should also replace "\\" in filenames with "\"
-	# if fv: ι = re.sub(r'\\ ',' ',ι); fv = re.match(r'^file://(.*)',ι)
-
-	fv = re.match(r'^file://(.*)',ι)
-	if app is None and fv:
-		app = "Path Finder" if os.path.isdir(fv.group(1)) else "Sublime Text"
-		if app is "Sublime Text": ι = fv.group(1)
+	if app is None and re.match(r'^file:',ι):
+		t = urllib.parse.unquote(re.sub(r'^file:(//)?','',ι))
+		if view and view.file_name() and t[0] != '/': t = os.path.normpath(os.path.join(os.path.dirname(view.file_name()),t)); ι = 'file:'+urllib.parse.quote(t.encode('utf-8'))
+		if os.path.isdir(t): app = "Path Finder"
+		elif os.path.splitext(t)[1] in ['.pdf','.m4a','.epub']: pass
+		else: app = "Sublime Text"
 
 	if app is "Terminal":
-		if os.path.isdir(ι):
-			t = "cd "+re.sub(r' ','\\\\\\\\ ',ι)+"; clear"
-		else:
-			dir,base = os.path.dirname(ι), os.path.basename(ι)
-			t = "cd "+sh_encode(dir)+"; clear; set -- "+sh_encode(base)+"; ({ sleep 0.01; printf \"\\b${green}/${purple}\""+sh_encode(base)+"\" ${reset}\"; } &)"
-		os.system("osascript -e 'tell application \"terminal\"' -e 'do script "+osa_encode(t)+"' -e 'end tell'"+("; osascript -e 'tell application \"terminal\" to activate'" if focus else ""))
+		dir,base = [ι,None] if os.path.isdir(ι) else [os.path.dirname(ι), os.path.basename(ι)]
+		t = "cd "+sh_encode(dir)+"; clear"
+		if base: t += "; set -- "+sh_encode(base)+'; ({ sleep 0.01; printf "\\b${green}/${purple}"'+sh_encode(base)+'" ${reset}"; } &)'
+		os.system("osascript -e "+sh_encode('tell app "terminal" to do script '+osa_encode(t)))
 	else:
-		print([ι for ι in ["open", app and "-a", app, not focus and "-g", ι] if ι])
 		subprocess.call([ι for ι in ["open", app and "-a", app, not focus and "-g", ι] if ι])
-
-	if app is "Path Finder": os.system("osascript -e 'tell application \"Path Finder\" to activate'")
+	if focus and app in ["Path Finder","Terminal"]: os.system("osascript -e 'tell app \""+app+"\" to activate'") # workaround for a bug
 
 def omnibox(ι): return ι if re.match(IS_URL,ι) else "https://www.google.com/search?q="+urllib.parse.quote(ι.encode("utf-8")) # +"&btnI=I"
 
@@ -90,21 +81,20 @@ class OpenContextCommand(sublime_plugin.TextCommand):
 				h = ['L'+str(view.rowcol(ι)[0] + 1) for ι in [view.sel()[0].begin(), view.sel()[-1].end()]]
 				open(t+('' if h[0] == 'L1' else '#'+(h[0] if h[0] == h[1] else h[0]+'-'+h[1])),focus=focus)
 		elif type == "terminal":
-			open(view.file_name(),focus=focus,app="Terminal")
+			open(view.file_name() or os.getenv("HOME"),focus=focus,app="Terminal")
 		elif type == "link":
 			if mouse: t = view.sel()[0]; ι = [] if not t.empty() else [ι for ι in [expand_empty_region_to_url(view, view.sel()[0], True)] if not ι.empty()]
 			else: ι = expand_empty_regions_to_urls_or_lines(view, view.sel())
-			for ι in ι: open(omnibox(view.substr(ι)),focus=focus)
+			if mouse and len(ι): view.sel().clear(); view.sel().add(sublime.Region(ι[0].end(),ι[0].end())) # workaround for a bug
+			for ι in ι: open(omnibox(view.substr(ι)),focus=focus,view=view)
 
 class InlineEvalZetaCommand(sublime_plugin.TextCommand):
 	def run(self,edit):
 		view = self.view; sel = view.sel()
 		append = len(sel) == 1 and sel[0].empty()
 		sel = expand_empty_regions_to_lines(view, sel)
-		vs = json.dumps([view.substr(ι) for ι in sel])
-		r = subprocess.check_output(['/usr/local/bin/node','--harmony','/usr/local/bin/ζ','--es',vs]).decode('utf-8')
-		r = re.sub(r'\x1b\[.*?m','',r) #! hack
-		r = r.split('DukKUhmtGonKdELGvFycnF0WTZXRGiJ2e1P1SBD5yg')
+		ι = [view.substr(ι) for ι in sel]
+		r = json.loads(subprocess.check_output(['/usr/local/bin/node','--harmony','/usr/local/bin/ζ','-pa',json.dumps(ι)]).decode('utf-8'))
 		if append:
 			P = '-> '
 			last = sublime.Region(sel[0].end(),sel[0].end())
@@ -116,16 +106,51 @@ class InlineEvalZetaCommand(sublime_plugin.TextCommand):
 			for i in range(len(sel))[::-1]:
 				view.replace(edit, sel[i], r[i])
 
-def compile_stuff(self,edit,toζ):
+def compile_stuff(self,edit,f):
 	view = self.view; sel = view.sel()
 	sel = expand_empty_region_to_whole_buffer(view, sel)
 	sel = expand_empty_regions_to_lines(view, sel)
 	for i in range(len(sel))[::-1]:
-		r = echo_string_pipe_process__check_output(view.substr(sel[i]), ['/usr/local/bin/node','--harmony','/usr/local/bin/ζ','-p','ζ_compile'+('["⁻¹"]' if toζ else '')+'(ι)'])
+		r = echo_string_pipe_process__check_output(view.substr(sel[i]), ['/usr/local/bin/node','--harmony','/usr/local/bin/ζ','-p',f+'(ι)'])
 		view.replace(edit, sel[i], r)
 
 class InlineCompileZetaJsCommand(sublime_plugin.TextCommand):
-	def run(self,edit): compile_stuff(self,edit,False)
-
+	def run(self,edit): compile_stuff(self,edit,'ζ_compile')
 class InlineCompileJsZetaCommand(sublime_plugin.TextCommand):
-	def run(self,edit): compile_stuff(self,edit,True)
+	def run(self,edit): compile_stuff(self,edit,'ζ_compile["⁻¹"]')
+
+class NiceUrlCommand(sublime_plugin.TextCommand):
+	def run(self,edit):
+		view = self.view
+		sel = expand_empty_regions_to_urls_or_lines(view, view.sel())
+		for region in sel:
+			ι = o = view.substr(region)
+
+			ι = re.sub(r'^https://','http://',ι)
+			ι = re.sub(r'^(http://)www\.',r'\1',ι)
+			ι = re.sub(r'^(http://)(?:mail\.)?(google\.com/mail/)u/0/\??#(?:(?:label|search)/[\w%+]+|\w+)/(\w+)',r'\1\2#all/\3',ι)
+			ι = re.sub(r'^(http://)en\.(wikipedia\.org)/',r'\1\2',ι)
+			ι = re.sub(r'^(http://)youtube\.com/watch\?ι=([^&]+)&',r'\1youtu.be/\2?',ι)
+			ι = re.sub(r'^(http://)youtube\.com/watch\?ι=([^&]+)' ,r'\1youtu.be/\2' ,ι)
+			ι = re.sub(r'^(http://)smile\.(amazon.com/)',r'\1\2',ι)
+			ι = re.sub(r'^(http://amazon.com/.*)\?sa-no-redirect=1$',r'\1',ι)
+			ι = re.sub(r'^(http://docs\.google\.com/document/d/[\w_-]+)/edit(?:\?ts=\w+)?$',r'\1',ι)
+			ι = re.sub(r'^(http://docs\.google\.com/spreadsheets/d/[\w_-]+)/edit(?:#gid=0)?$',r'\1',ι)
+			ι = re.sub(r'^(http://dropbox.com/.*)\?dl=0$',r'\1',ι)
+			ι = re.sub(r'^(http://)facebook(\.com/)',r'\1\2',ι)
+			ι = re.sub(r'^(http://fb\.com/[\w.]+)([?&]fref=\w+)?([?&]hc_location=[\w_]+)?',r'\1',ι)
+			ι = re.sub(r'^"(.*)"$',r'“\1”',ι) #! not actually for urls
+
+			if ι is not o: view.replace(edit, region, ι)
+
+############ todo ############
+
+# needs: parsing the query string
+# http://amazon.com/Lower-Your-Taxes-Time-2013-2014/dp/0071803408/ref=sr_1_1?s=books&ie=UTF8&qid=1365921208&sr=1-1&keywords=Lower+Your+Taxes%3ABig+Time
+# http://amazon.com/Lower-Your-Taxes-Time-2013-2014/dp/0071803408
+
+# http://smile.amazon.com/Thinking-Pencil-Henning-Nelms/dp/0898150523?sa-no-redirect=1
+# Thinking Pencil http://amzn.to/1jxKp9q
+
+# https://docs.google.com/spreadsheets/d/1wfFMPo8n_mpcoBCFdsIUUIt7oSm7d__Duex51yejbBQ/edit#gid=0
+# http://goo.gl/0nrUfP
